008cbd5e0944a5c083b584f6c5bd3c51633a527d
Дмитрий Мухин <SawHo@DESKTOP-4H8896D>
2018-02-24 23:23:30 +0700

    Создать 1 массив объектов , заполнил его объектами Range, и потом выдал из метода.

---
+++ CourseTasks/Range/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Range r1 = new Range(23, 32);
+
+            double numberInRange = 8;
+            r1.IsInside(numberInRange);
+
+            Range r2 = new Range(28, 35);
+
+            if (r1.GetGeneral(r2) != null)
+            {
+                Range r3;
+                r3 = r1.GetGeneral(r2);
+                Console.WriteLine("General Interval:" + " " + r3.From + " " + r3.To);
+            }
+            else
+            {
+                Console.WriteLine("Null");
+            }
+
+            Range[] arrayResult;
+            arrayResult = r1.GetArrayOfObject(r2);
+
+            for (int i = 0; i < arrayResult.Length; i++)
+            {
+                Console.WriteLine("Begin: " + arrayResult[i].From + " End: " + arrayResult[i].To);
+            }
+
+        }
+    }
+}
---
+++ CourseTasks/Range/Range.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Range
+    {
+        public double From { get; set; }
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double Length
+        {
+            get { return To - From; }
+        }
+
+        public bool IsInside(double numberInRange)
+        {
+            return From <= numberInRange && To >= numberInRange;
+        }
+
+        public Range GetGeneral(Range r2)
+        {
+            if ((r2.From >= To) || (From >= r2.To))
+            {
+                return null;
+            }
+            else
+            {
+                if (From >= r2.From && To >= r2.To)
+                {
+                    return new Range(From, r2.To);
+                }
+                else if (To <= r2.To && r2.From >= From)
+                {
+                    return new Range(r2.From, To);
+                }
+                else if (From <= r2.From && To >= r2.To)
+                {
+                    return new Range(r2.From, r2.To);
+                }
+                else
+                {
+                    return new Range(From, To);
+                }
+            }
+        }
+
+        public Range[] GetArrayOfObject(Range r2)
+        {
+
+            int x;
+            if ((r2.From >= To) || (From >= r2.To))
+            {
+                x = 2;
+            }
+            else
+            {
+                x = 1;
+            }
+
+            Range[] arrayOfRanges = new Range[x];
+
+            if (x == 2)
+            {
+                Range r = new Range(From, To);
+                arrayOfRanges[0] = r;
+                arrayOfRanges[1] = r2;
+            }
+            else
+            {
+                if (From >= r2.From && To >= r2.To)
+                {
+                    Range r = new Range(From, r2.To);
+                    arrayOfRanges[0] = r;
+                }
+                else if (To <= r2.To && r2.From >= From)
+                {
+                    Range r = new Range(r2.From, To);
+                    arrayOfRanges[0] = r;
+                }
+                else if (From <= r2.From && To >= r2.To)
+                {
+                    Range r = new Range(r2.From, r2.To);
+                    arrayOfRanges[0] = r;
+                }
+                else
+                {
+                    Range r = new Range(From, To);
+                    arrayOfRanges[0] = r;
+                }
+            }
+            return arrayOfRanges;
+        }
+    }
+}
